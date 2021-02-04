cc88080e5c38ed68bc51bc61d2e8ffe5f5cf76dd
root <root@DESKTOP-LSDMKDN.localdomain>
2021-02-04 13:49:24 +0100

    add-ons

--- script.js
+++ script.js
@@ -9,27 +9,39 @@ about.on("click", function aboutClick(e) {
 });
 
 var more = $(".more");
-// more.on("click", function aboutClick(e) {
-//   var moretxt = $(".more-txt");
-//   moretxt.css("visibility", "visible");
-// });
 
+more.on("click", function aboutClick(e) {
+  console.log("ej ju");
+  var moretxt = $(".more-txt");
+  moretxt.css("visibility", "visible");
+});
+
+var dark = $(".dark");
 var board = $(".board");
+
+dark.on("click", function aboutClick(e) {
+  console.log("jes ju");
+  $(board).css("background-color", "black");
+});
+
 var slot = $(".slot");
 var img = $("img");
 //var s1 = new Audio("./shum1.wav");
 var s2 = new Audio("./popfol1.wav");
 
 slot.on("click", function mouseClick(e) {
+  event.stopPropagation(e);
   $(e.currentTarget).css("visibility", "hidden");
   s2.play(e);
 });
 
 board.hover(function (e) {
+  event.stopPropagation(e);
   slot.toggleClass("mouseon");
 });
 
 img.on("mouseenter", function mouseOver(e) {
+  event.stopPropagation(e);
   console.log("mouseover");
   $(e.currentTarget).toggleClass("rotateone");
 });
